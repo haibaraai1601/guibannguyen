e0e41de7c467798684f9b07e1d2420072963df48
haibaraai1601 <88544865+haibaraai1601@users.noreply.github.com>
2021-08-12 19:46:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'hello bạn Nguyên!',
-    introDesc: `~~~~~~`,
-    btnIntro: 'bấm vào đi',
-    title: 'Hỏi thiệt nghen  😙',
-    desc: 'bạn có bị khùng hông ',
-    btnYes: 'có, tui bị khùng á <33',
-    btnNo: 'Không,tui là Nguyên xinh đẹp,ngoan,hiền',
-    question:'sao bạn bị khùng dậy <3',
-    btnReply: 'Gửi cho anh Vũ<3',
-    reply: ' tui nói thiệt tui bị khùng',
-    mess: 'tui biết bạn bị khùng mà  🥰😘😘',
-    messDesc: '.....',
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
